e7dfa066667f6b85c3fdbc2c5e2d5836f9016363
Mizanur Rahman <36396771+mizanur75@users.noreply.github.com>
2024-01-23 21:11:30 +0600

    create index page and form design

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+  session_start(); 
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Grab Excel Data</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container">
+      <h1 class="text-center m-5">Grab Excel Data</h1>
+      <?php
+        if(isset($_SESSION['success'])){
+        ?>
+          <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <?php echo $_SESSION['success']; ?>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+          </div>
+       
+       <?php 
+       }
+        unset($_SESSION['success']);
+        
+      ?>
+      <?php
+        if(isset($_SESSION['error'])){
+        ?>
+          <div class="alert alert-warning alert-dismissible fade show" role="alert">
+            <?php echo $_SESSION['error']; ?>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+          </div>
+       
+       <?php 
+       }
+         unset($_SESSION['error']);
+      ?>
+      <form action="process.php" method="POST" enctype="multipart/form-data">
+        <div class="row">
+          <div class="input-group mb-3">
+            <input type="file" name="excel_file" class="form-control" id="inputGroupFile02">
+            <label class="input-group-text" for="inputGroupFile02">Upload</label>
+          </div>
+          <input type="submit" name="submit" class="btn btn-success" value="Upload">
+        </div>
+      </form>
+
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  </body>
+</html>
